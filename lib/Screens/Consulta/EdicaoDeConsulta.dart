06454e8cdaafcda5278c51c1abc04647e54c68f1
Paulo Henrique Pereira Oliveira <paulohenrique81@gmail.com>
2020-01-31 14:00:57 -0300

    Alteração no botão de voltar, na tela de edição de consulta

--- lib/Screens/Consulta/EdicaoDeConsulta.dart
+++ lib/Screens/Consulta/EdicaoDeConsulta.dart
@@ -93,154 +93,191 @@ class _EdicaoDeConsultaState extends State<EdicaoDeConsulta> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text(_consultaEdicao.nomeDoMedico ?? "Nova Consulta"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (_formKey.currentState.validate()) {
-            if (_novaConsulta == true) {
-              await conectionDB.cadastraConsulta(
-                  widget.user.uid,
-                  _nomeMedicoController.text,
-                  _dataController.text,
-                  _horaController.text,
-                  _localController.text);
-            } else {
-              await conectionDB.atualizarConsulta(
-                  widget.user.uid,
-                  _consultaEdicao.idConsulta,
-                  _nomeMedicoController.text,
-                  _dataController.text,
-                  _horaController.text,
-                  _localController.text,
-                  especialidade: _especialidadeController.text,
-                  diagnostico: _diagnosticoController.text,
-                  exames: _examesController.text,
-                  medicamentos: _medicamentosController.text,
-                  formaDePagamento: _formaDePagamentoController.text,
-                  valor: _valorController.text);
-            }
+    return WillPopScope(
+        onWillPop: _requestPop,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurple,
+            title: Text(_consultaEdicao.nomeDoMedico ?? "Nova Consulta"),
+            centerTitle: true,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                if (_novaConsulta == true) {
+                  await conectionDB.cadastraConsulta(
+                      widget.user.uid,
+                      _nomeMedicoController.text,
+                      _dataController.text,
+                      _horaController.text,
+                      _localController.text);
+                } else {
+                  await conectionDB.atualizarConsulta(
+                      widget.user.uid,
+                      _consultaEdicao.idConsulta,
+                      _nomeMedicoController.text,
+                      _dataController.text,
+                      _horaController.text,
+                      _localController.text,
+                      especialidade: _especialidadeController.text,
+                      diagnostico: _diagnosticoController.text,
+                      exames: _examesController.text,
+                      medicamentos: _medicamentosController.text,
+                      formaDePagamento: _formaDePagamentoController.text,
+                      valor: _valorController.text);
+                }
 
-            Navigator.pop(context);
-          }
-        },
-        child: Icon(Icons.save),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: SingleChildScrollView(
-          padding: EdgeInsets.all(10.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: _nomeMedicoController,
-                  decoration: InputDecoration(labelText: "Nome do Médico:"),
-                  validator: (val) =>
-                      val.isEmpty ? 'Digite o nome do Médico' : null,
-                  onChanged: (text) {
-                    _userEdited = true;
-                    setState(() {
-                      _consultaEdicao.nomeDoMedico = text;
-                    });
+                Navigator.pop(context);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.deepPurple,
+          ),
+          body: SingleChildScrollView(
+              padding: EdgeInsets.all(10.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _nomeMedicoController,
+                      decoration: InputDecoration(labelText: "Nome do Médico:"),
+                      validator: (val) =>
+                          val.isEmpty ? 'Digite o nome do Médico' : null,
+                      onChanged: (text) {
+                        _userEdited = true;
+                        setState(() {
+                          _consultaEdicao.nomeDoMedico = text;
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: _especialidadeController,
+                      decoration: InputDecoration(
+                          labelText: "Especialidade do Médico:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.especialidade = text;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _dataController,
+                      decoration: InputDecoration(labelText: "Data:"),
+                      validator: (val) => val.isEmpty ? 'Digite a data' : null,
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.data = text;
+                      },
+                      onTap: () => _selectDate(context),
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextFormField(
+                      controller: _horaController,
+                      decoration: InputDecoration(labelText: "Horário:"),
+                      validator: (val) =>
+                          val.isEmpty ? 'Digite o horário' : null,
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.horario = text;
+                      },
+                      onTap: () => _selectTime(context),
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextFormField(
+                      controller: _localController,
+                      decoration: InputDecoration(labelText: "Local:"),
+                      validator: (val) => val.isEmpty ? 'Digite o local' : null,
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.local = text;
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Text(
+                        "Pós consulta",
+                        style: TextStyle(fontSize: 22.0),
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _diagnosticoController,
+                      decoration:
+                          InputDecoration(labelText: "Diagnóstico dado:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.diagnostico = text;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _examesController,
+                      decoration:
+                          InputDecoration(labelText: "Exames solicitados:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.exames = text;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _medicamentosController,
+                      decoration: InputDecoration(
+                          labelText: "Medicamentos receitados:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.medicamentos = text;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _formaDePagamentoController,
+                      decoration:
+                          InputDecoration(labelText: "Forma de pagamento:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.formaDePagamento = text;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _valorController,
+                      decoration: InputDecoration(labelText: "Valor:"),
+                      onChanged: (text) {
+                        _userEdited = true;
+                        _consultaEdicao.valor = text;
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
+                ),
+              )),
+        ));
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Descartar alterações?"),
+              content: Text("Se sair as alterações serão perdidas."),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Cancelar"),
+                  onPressed: () {
+                    Navigator.pop(context);
                   },
                 ),
-                TextField(
-                  controller: _especialidadeController,
-                  decoration:
-                      InputDecoration(labelText: "Especialidade do Médico:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.especialidade = text;
+                FlatButton(
+                  child: Text("Sim"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
                   },
-                ),
-                TextFormField(
-                  controller: _dataController,
-                  decoration: InputDecoration(labelText: "Data:"),
-                  validator: (val) => val.isEmpty ? 'Digite a data' : null,
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.data = text;
-                  },
-                  onTap: () => _selectDate(context),
-                  keyboardType: TextInputType.datetime,
-                ),
-                TextFormField(
-                  controller: _horaController,
-                  decoration: InputDecoration(labelText: "Horário:"),
-                  validator: (val) => val.isEmpty ? 'Digite o horário' : null,
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.horario = text;
-                  },
-                  onTap: () => _selectTime(context),
-                  keyboardType: TextInputType.datetime,
-                ),
-                TextFormField(
-                  controller: _localController,
-                  decoration: InputDecoration(labelText: "Local:"),
-                  validator: (val) => val.isEmpty ? 'Digite o local' : null,
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.local = text;
-                  },
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Text(
-                    "Pós consulta",
-                    style: TextStyle(fontSize: 22.0),
-                  ),
-                ),
-                TextFormField(
-                  controller: _diagnosticoController,
-                  decoration: InputDecoration(labelText: "Diagnóstico dado:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.diagnostico = text;
-                  },
-                ),
-                TextFormField(
-                  controller: _examesController,
-                  decoration: InputDecoration(labelText: "Exames solicitados:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.exames = text;
-                  },
-                ),
-                TextFormField(
-                  controller: _medicamentosController,
-                  decoration:
-                      InputDecoration(labelText: "Medicamentos receitados:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.medicamentos = text;
-                  },
-                ),
-                TextFormField(
-                  controller: _formaDePagamentoController,
-                  decoration: InputDecoration(labelText: "Forma de pagamento:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.formaDePagamento = text;
-                  },
-                ),
-                TextFormField(
-                  controller: _valorController,
-                  decoration: InputDecoration(labelText: "Valor:"),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _consultaEdicao.valor = text;
-                  },
-                  keyboardType: TextInputType.number,
-                ),
+                )
               ],
-            ),
-          )),
-    );
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
